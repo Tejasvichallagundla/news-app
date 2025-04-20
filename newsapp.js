8048f792e70a6386a02ffcf065f147d5cc74bb06
Tejasvi Challagundla <tejasvichallagundla24@gmail.com>
2025-04-20 13:42:02 +0530

    updated

--- newsapp.js
+++ newsapp.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
 async function fetchfunction(){
     const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
@@ -15,3 +16,27 @@ async function fetchfunction(){
     
 }
 
+=======
+const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
+async function fetchfunction(){
+    const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
+    const category=document.getElementById("select").value;
+    const response=await fetch(`https://gnews.io/api/v4/top-headlines?&lang=en&country=in&category=${category}&apikey=${API_KEY}`);
+    const data=await response.json();
+    console.log(data);
+    data.articles.forEach(element => {
+
+        newsvalue=document.getElementById("news"); 
+        newsvalue.innerHTML +=
+        `<div style="background-color: beige; margin:20px;">
+        <p>${element.title}</p>
+        <p>${element.description}</p>
+        <img src=${element.image} height:"300" width="300">
+        <a href=${element.url}>read more</a>
+        </div>`
+        console.log(element.title);
+    });
+    
+}
+
+>>>>>>> ea1a5ed (updated)
