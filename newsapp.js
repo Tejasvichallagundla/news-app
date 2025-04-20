d04ec46c3152021876f7f63932fcdb920d97be8f
Tejasvi Challagundla <tejasvichallagundla24@gmail.com>
2025-04-20 20:31:31 +0530

    updated the applicatioin

--- newsapp.js
+++ newsapp.js
@@ -1,42 +1,25 @@
-<<<<<<< HEAD
 const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
 async function fetchfunction(){
     const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
+    const query=document.getElementById("q").value;
     const category=document.getElementById("select").value;
-    const response=await fetch(`https://gnews.io/api/v4/top-headlines?&lang=en&country=in&category=${category}&apikey=${API_KEY}`);
+    const response=await fetch(`https://gnews.io/api/v4/top-headlines?q=${query}&lang=en&country=in&category=${category}&apikey=${API_KEY}`);
     const data=await response.json();
     console.log(data);
     data.articles.forEach(element => {
-
+        
         newsvalue=document.getElementById("news"); 
         newsvalue.innerHTML +=
-        `<p>${element.title}</p>`
-        console.log(element.title);
-    });
-    
-}
-
-=======
-const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
-async function fetchfunction(){
-    const API_KEY= "358e4b45aecc5fb7a8b1c20d2773500f";
-    const category=document.getElementById("select").value;
-    const response=await fetch(`https://gnews.io/api/v4/top-headlines?&lang=en&country=in&category=${category}&apikey=${API_KEY}`);
-    const data=await response.json();
-    console.log(data);
-    data.articles.forEach(element => {
-
-        newsvalue=document.getElementById("news"); 
-        newsvalue.innerHTML +=
-        `<div style="background-color: beige; margin:20px;">
-        <p>${element.title}</p>
+        `<div class="newscard">
+        <div class="imagediv">
+        <img src=${element.image} style="height:300px; width:auto;">
+        </div>
+        <h2>${element.title}</h2>
         <p>${element.description}</p>
-        <img src=${element.image} height:"300" width="300">
-        <a href=${element.url}>read more</a>
+        <button><a href=${element.url}>Read more</a></button>
         </div>`
         console.log(element.title);
     });
     
 }
 
->>>>>>> ea1a5ed (updated)
